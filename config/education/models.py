6d9205d46238ca681d81715ed4a33c9c2c29e0dd
kanlar75 <kanlar75@gmail.com>
2023-10-11 19:02:35 +0300

    hw

--- config/education/models.py
+++ config/education/models.py
@@ -26,6 +26,9 @@ class Lesson(models.Model):
     picture = models.ImageField(upload_to='education/', **NULLABLE,
                                 verbose_name='превью')
     link = models.URLField(max_length=200, **NULLABLE, verbose_name='ссылка')
+    course = models.ForeignKey('Course', on_delete=models.CASCADE,
+                               related_name='lesson', verbose_name='курс',
+                               **NULLABLE)
 
     def __str__(self):
         return f'{self.title}'
@@ -33,3 +36,31 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = 'урок'
         verbose_name_plural = 'уроки'
+
+
+class Payment(models.Model):
+    CASH = "Наличные"
+    CARD = "Безналичные"
+
+    PAYMENT_CHOICES = [
+        (CASH, "Наличные"),
+        (CARD, "Безналичные"),
+    ]
+
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE,
+                             verbose_name='пользователь')
+    date_of_payment = models.DateField(verbose_name='дата платежа')
+    course = models.ForeignKey('Course', on_delete=models.CASCADE,
+                               verbose_name='оплаченный курс')
+    lesson = models.ForeignKey('Lesson', on_delete=models.CASCADE,
+                               verbose_name='оплаченный урок')
+    sum_of_payment = models.IntegerField(verbose_name='сумма платежа')
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_CHOICES,
+                                      verbose_name='способ оплаты')
+
+    def __str__(self):
+        return f'{self.date_of_payment} - {self.sum_of_payment}'
+
+    class Meta:
+        verbose_name = 'платёж'
+        verbose_name_plural = 'платежи'
